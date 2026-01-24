265adf57ed6cebbf4b72bc8f4cfa78bb50e7cef5
Atticus F. <71795010+beackers@users.noreply.github.com>
2026-01-24 11:56:58 -0600

    couple of things
    1. print if less than 100 items
    Because the tests with 8 integers print over 100000 lines.
    2. get ready for exponents
    change everything over from raw numbers to number "objects" which
    contain value, exponent value, and history (thinking ahead), and add
    calc.eval, which takes these objects.
    Current state works.

--- calculations.lua
+++ calculations.lua
@@ -1,5 +1,9 @@
 local M = {}
+function M.eval(n)
+  return n.value ^ n.exp
+end
 function M.raws(a, b)
+  a, b = M.eval(a), M.eval(b)
   return {
     a + b,
     a * b,
--- knockout.lua
+++ knockout.lua
@@ -3,6 +3,7 @@ local calc = require("calculations")
 -- statistics (for nerds)
 local stats = {
   calls = 0,
+  rawcalls = 0,
   pruned = 0,
   leaves = 0
 }
@@ -14,7 +15,11 @@ stats.uniqueStates = {}
 local numbers = {}
 
 for i = 1, #arg do
-  table.insert(numbers, tonumber(arg[i]))
+  table.insert(numbers, {
+    value = tonumber(arg[i]),
+    exp = 1,
+    history = {}
+  })
 end
 
 -- normalize keys
@@ -22,25 +27,44 @@ local function normalize(number)
   return tostring(math.tointeger(number))
 end
 
--- unique state
+-- unique state id
+local function numberKey(n)
+  return tostring(n.value) .. "^" .. tostring(n.exp)
+end
+
 local function stateKey(numbers)
   local tmp = {}
   for i = 1, #numbers do
-    tmp[#tmp+1] = normalize(numbers[i])
+    tmp[#tmp+1] = numbers[i]
   end
-  table.sort(tmp)
-  return table.concat(tmp, ",")
+
+  table.sort(tmp, function(a, b)
+    if a.value ~= b.value then
+      return a.value < b.value
+    else
+      return a.exp < b.exp
+    end
+  end)
+
+  local parts = {}
+  for i = 1, #tmp do
+    parts[#parts+1] = "(" .. numberKey(tmp[i]) .. ")"
+  end
+
+  return table.concat(parts, ",")
 end
 
 
 -- calculations
 local results = {}
+local allowedExps = {0, 0.5, 1, 2}
 local function search(numbers, depth)
+  stats.rawcalls = stats.rawcalls + 1
   -- base case n=1
   local n = #numbers
   if n == 1 then
     stats.leaves = stats.leaves + 1
-    results[numbers[1]] = true
+    results[calc.eval(numbers[1])] = true
     return
   end
 
@@ -63,23 +87,32 @@ local function search(numbers, depth)
     for j = i + 1, n do
       local a, b = numbers[i], numbers[j]
 
-      -- calculate everything possible
+      -- calculate with all operations
       for _, r in ipairs(calc.raws(a, b)) do
+        -- if we like r, then
         if r and math.abs(math.floor(r)) == r and r ~= math.huge and r ~= -math.huge then
-          -- copy numbers but replace i and j with r
+          -- copy numbers without i or j
           local next = {}
           for k = 1, n do
             if k ~= i and k ~= j then
               next[#next+1] = numbers[k]
             end
           end
-          next[#next+1] = r
+
+          -- add r
+          next[#next+1] = {
+            value = r,
+            exp = 1,
+            history = {}
+          }
+
+          -- go to the next level in the search
           search(next, depth+1)
-        end
-      end
-    end
-  end
-end
+        end -- we like r if
+      end -- for each value of r
+    end -- j index end
+  end -- i index end
+end -- fn end
 
 search(numbers)
 
@@ -88,21 +121,27 @@ local out = {}
 for r in pairs(results) do
   out[#out+1] = r
 end
-table.sort(out)
+table.sort(out, function(a, b)
+  return a < b
+end
+)
 
 -- uniquing
 -- skipping because we set, not list, so everything's already unique
 
--- printing
-for _, r in ipairs(out) do
-  print(
-    r
-  )
+-- printing (under 100)
+if #out < 100 then
+  for _, r in ipairs(out) do
+    print(
+      r
+    )
+  end
 end
 
 
 print("~~~~~ stats for nerds ~~~~~~~")
 print("calls:", stats.calls)
+print("raw calls to search():", stats.rawcalls)
 print("leaves:", stats.leaves)
 for d = 1, #stats.depth do
   print("depth ", d, stats.depth[d] or 0)
